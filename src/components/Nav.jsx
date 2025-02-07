fdbb82772bcbe2cdf6b203f4570c8ec2762b4ced
candela-greco <candelagreco15@gmail.com>
2025-02-07 13:58:05 -0300

    Modifique el dropdown menu para que pueda usar la clase active y funcione igual al resto de los links del nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink } from "react-router-dom";
 import { useState } from 'react';
 
 function NavbarNav() {
@@ -10,30 +10,33 @@ function NavbarNav() {
     return (
         <Navbar expand="lg" className="Navbar">
             <Container fluid className="NavbarContainer">
-                {/* Marca separada del menú */}
-                <Navbar.Brand href="#home" className="Brand">NEXON</Navbar.Brand>
-
-                {/* Toggle (Botón de hamburguesa) */}
+                <Navbar.Brand href="/" className="Brand">NEXON</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" className="border-white ms-auto"/>
 
-                {/* Menú de navegación alineado a la derecha */}
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#features">Inicio</Nav.Link>
-                        <Nav.Link href="#pricing">Productos</Nav.Link>
+                        <Nav.Link as={NavLink} to="/">Inicio</Nav.Link>
+                        <Nav.Link as={NavLink} to="/productos">Productos</Nav.Link>
 
-                        <NavDropdown
-                            title="Mi cuenta"
-                            className="Dropdown"
-                            show={showDropdown}
+                        {/* Contenedor personalizado para el dropdown */}
+                        <Nav.Item
+                            className="custom-dropdown"
                             onMouseEnter={() => setShowDropdown(true)}
                             onMouseLeave={() => setShowDropdown(false)}
                         >
-                            <NavDropdown.Item href="#action/3.1">Iniciar Sesión</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Registrarse</NavDropdown.Item>
-                        </NavDropdown>
-                        
-                        <Nav.Link href="#deets">Carrito</Nav.Link>
+                            {/* NavLink en "Mi cuenta" */}
+                            <Nav.Link as={NavLink} to="/login" className="nav-link">
+                                Mi cuenta
+                            </Nav.Link>
+                            
+                            {/* Dropdown personalizado */}
+                            <div className={`dropdown-menu ${showDropdown ? "show" : ""}`}>
+                                <NavLink className="dropdown-item" to="/login">Iniciar Sesión</NavLink>
+                                <NavLink className="dropdown-item" to="/register">Registrarse</NavLink>
+                            </div>
+                        </Nav.Item>
+
+                        <Nav.Link as={NavLink} to="/carrito">Carrito</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -42,3 +45,4 @@ function NavbarNav() {
 }
 
 export default NavbarNav;
+
