5592166c1690f68afd5b905a9f5fa1b24a007f83
candela-greco <candelagreco15@gmail.com>
2025-02-08 16:54:40 -0300

    Configuracion rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <BrowserRouter basename="/nexon.github.io">
       <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
