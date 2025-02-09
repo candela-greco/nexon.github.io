2cab8575815526962bdd508f2f5d202666753ef6
candela-greco <candelagreco15@gmail.com>
2025-02-09 14:51:54 -0300

    Modificacion App.jsx Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import NavbarNav from "./components/Nav.jsx";
 import Home from "./components/Home.jsx";
@@ -9,14 +9,14 @@ import Products from "./components/Products.jsx";
 
 function App() {
   return (
-    <Router>
+    <>
       <NavbarNav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productos" element={<Products />} />
       </Routes>
       <Footer />
-    </Router>
+    </>
   );
 }
 
